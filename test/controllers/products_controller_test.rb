4cda4ed72da63cf780ad474dbf62abf00697d02a
Jason C <Jason C>
2020-10-13 17:26:48 -0400

    2020-10-13-D3 LineItem test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,4 +46,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "cannot delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(@product)
+    end
+
+    assert_redirected_to products_url
+  end
+    
 end
