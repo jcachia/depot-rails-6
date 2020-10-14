1176368a1c0967b840dec8fc73d3bc1b3de10133
Jason C <Jason C>
2020-10-14 15:41:43 -0400

    2020-10-14-E4 line item removal tests

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @line_item = line_items(:one)
+  end
+
+  test "remove_one_item! reduces the line_item quantity by 1 and saves if quantity >1" do
+    @line_item.quantity = 4
+    @line_item.remove_one_item!
+    @line_item.reload
+    assert_equal @line_item.quantity, 3
+  end
+
+  test "remove_one_item! deletes the item if quantity == 1" do
+    @line_item.quantity = 1
+    assert_difference('LineItem.count', -1) do
+      @line_item.remove_one_item!
+    end
+  end
 end
