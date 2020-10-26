f47f82cd5dc305294062455d50b35f6285a2c253
Jason C <Jason C>
2020-10-26 19:15:16 -0400

    2020-10-26-G1 correct method order in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order < ApplicationRecord
-
-  validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
-
   enum pay_type: {
     "Check"           => 0,
     "Credit Card"     => 1,
     "Purchase Order"  => 2
   }
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
+
 end
