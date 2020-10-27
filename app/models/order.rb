ab5900cd5b48d528411daa6932c1aae591688196
Jason C <Jason C>
2020-10-27 19:00:10 -0400

    2020-10-27-G1 set up order and line item relation

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,5 +7,11 @@ class Order < ApplicationRecord
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
 
+  def self.add_line_items_to_cart(cart)
+    cart.line_item.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 
 end
