d22e1d39d77f33a35952c248b76a12f91a392cbd
Jason C <Jason C>
2020-11-12 08:52:48 -0500

    2020-11-11-H3 complete cart system test

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -10,6 +10,31 @@ class CartsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Carts"
   end
 
+  test "showing the cart when an item added" do
+    visit store_index_url
+    # no cart visible (present, but hidden)
+    assert_selector '#cart', visible: :false
+
+    click_on 'Add to Cart', match: :first
+    # cart present with all buttons
+    assert_selector '#cart'
+    assert_selector 'input[value="Remove item"]'
+    assert_selector 'input[value="Empty cart"]'
+    assert_selector 'input[value="Checkout"]'
+    # remove only the checkout button when taken to order screen.
+    click_on 'Checkout'
+    assert_selector '#cart'
+    assert_selector 'input[value="Remove item"]'
+    assert_selector 'input[value="Empty cart"]'
+    assert_no_selector 'input[value="Checkout"]'
+    # emptying the cart hides it
+    page.accept_confirm do
+      click_on 'Empty cart'
+    end
+    assert_selector '#cart', visible: :false
+
+  end
+
   # test "creating a Cart" do
   #   visit carts_url
   #   click_on "New Cart"
