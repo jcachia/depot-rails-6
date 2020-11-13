cef7307bcd973a306f3ee800ce2f7f85b461996e
Jason C <Jason C>
2020-11-13 11:03:32 -0500

    2020-11-13-I1 application mailer configs

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp # or :sendmail or :test.  sendmail will send to the local system's sendmail program, assumed to be in /usr/sbin.
+
+  # Some Gmail defaults:
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "rails6bookstore.com",
+    authenticaion:  "public",
+    user_name:      "jason",
+    password:       "secret",
+    enable_starttls_auto: true
+  }
+end
