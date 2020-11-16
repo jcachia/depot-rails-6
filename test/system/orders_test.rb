b23b02c36ed9ded49abc8025773f2175aed97148
Jason C <Jason C>
2020-11-16 16:29:52 -0500

    2020-11-16-I2 order system test part 1

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper # provides perform_enqueued_jobs.
+
   setup do
     @order = orders(:one)
   end
@@ -11,6 +13,10 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test "check for correct dynamic fields for pay types" do
+    # Make sure there are no lingering orders from prior tests.
+    LineItem.delete_all
+    Order.delete_all
+
     check_fields = %w{ #order_routing_number #order_account_number }
     credit_card_fields = %w{ #order_credit_card_number #order_expiration_date }
     po_fields = %w{ #order_po_number }
@@ -39,21 +45,46 @@ class OrdersTest < ApplicationSystemTestCase
       assert_no_selector selector
     end
 
-    select 'Credit Card', from: 'Pay Type'
-    credit_card_fields.each do |selector|
-      assert_selector selector
-    end
-    (check_fields + po_fields).each do |selector|
-      assert_no_selector selector
+    fill_in "Routing Number", with: "12345"
+    fill_in "Account Number", with: "98765"
+
+    # Initiating the action click button inside the perform_enqued_jobs block will process the job thats queued from the action.
+    perform_enqueued_jobs do
+      click_button "Place Order"
     end
 
-    select 'Purchase Order', from: 'Pay Type'
-    po_fields.each do |selector|
-      assert_selector selector
-    end
-    (check_fields + credit_card_fields).each do |selector|
-      assert_no_selector selector
-    end
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Order Name",        order.name
+    assert_equal "123 Order St.",     order.address
+    assert_equal "order@shopper.com", order.email
+    assert_equal "Check",             order.pay_type
+    assert_equal 1,                   order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last # Mail not sent in test; instead found in this array.
+    assert_equal ["order@shopper.com"], mail.to
+    assert_equal "BookStore Orders <orders@rails6bookstore.com>", mail[:from].value
+    assert_equal "Rails 6 BookStore Order Confirmation", mail.subject
+
+    # Will need a refactor here.
+    # select 'Credit Card', from: 'Pay Type'
+    # credit_card_fields.each do |selector|
+    #   assert_selector selector
+    # end
+    # (check_fields + po_fields).each do |selector|
+    #   assert_no_selector selector
+    # end
+    #
+    # select 'Purchase Order', from: 'Pay Type'
+    # po_fields.each do |selector|
+    #   assert_selector selector
+    # end
+    # (check_fields + credit_card_fields).each do |selector|
+    #   assert_no_selector selector
+    # end
 
   end
 
