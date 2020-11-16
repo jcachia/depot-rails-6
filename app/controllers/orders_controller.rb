0de143c2d736c8e7ff1c18f10bd3ec2799ab091c
Jason C <Jason C>
2020-11-16 16:28:10 -0500

    2020-11-16-I2 update order controller to submit charge ordr job

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id]) # why not @cart.destroy? we should already have the right cart.
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: "Thank you for your order!  You're awesome!" }
         format.json { render :show, status: :created, location: @order }
       else
