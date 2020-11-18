5bf015711d031c9b0352785231ab527f2d00815c
Jason C <Jason C>
2020-11-17 21:04:57 -0500

    2020-11-17 add ship_date to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -96,7 +96,7 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:name, :address, :email, :pay_type)
+      params.require(:order).permit(:name, :address, :email, :pay_type, :ship_date)
     end
 
 end
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! order, :id, :name, :address, :email, :pay_type, :created_at, :updated_at
+json.extract! order, :id, :name, :address, :email, :pay_type, :ship_date, :created_at, :updated_at
 json.url order_url(order, format: :json)
