12d90d6d144e983da9e193bbde4d70de7c31608f
Jason C <jason44406@yahoo.com>
2020-11-18 20:10:13 -0500

    2020-11-18-I3 rescue error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :notify_admin
+
+    private
+
+  def notify_admin(exception)
+    AdminMailer.notify_error(exception).deliver_now
+    redirect_to store_error_url
+  end
+
 end
