46cacb018fe818f9f4dd603c14b41a890a311325
Jason C <jason44406@yahoo.com>
2020-12-07 20:36:19 -0500

    2020-12-07-J3 skip auth where appropriate on controllers

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   include VerifyCart
+
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize, only: [:create, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
